eb38ba9b5f6e2913028e7847f71779737393651f
pranit96 <pranitthorve@gmail.com>
2025-10-15 11:47:52 +0530

    lets goo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        sans: ['"Manrope Variable"', 'Manrope', 'ui-sans-serif', 'system-ui'],
+        sans: ['Manrope','Rubik','Funnel Sans', 'ui-sans-serif', 'system-ui'],
       },
       colors: {
         border: "hsl(var(--border))",
